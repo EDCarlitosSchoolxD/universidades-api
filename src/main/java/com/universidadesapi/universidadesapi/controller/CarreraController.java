778e4570d754a44c82143503c748545d9d752f07
EDCarlitosSchoolxD <uxdj050728hqrczna1@cbtis111.edu.mx>
2023-02-04 16:57:53 -0500

    CarreraController(CRUD)

---
+++ src/main/java/com/universidadesapi/universidadesapi/controller/CarreraController.java
@@ -0,0 +1,49 @@
+package com.universidadesapi.universidadesapi.controller;
+
+import com.universidadesapi.universidadesapi.entity.Carrera;
+import com.universidadesapi.universidadesapi.service.CarreraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carreras")
+public class CarreraController {
+
+    @Autowired
+    CarreraService carreraService;
+
+
+    @GetMapping("/")
+    public List<Carrera> getAll(){
+        return carreraService.getAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Carrera> getOne(@PathVariable Long id){
+        return carreraService.getOne(id );
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Carrera> save(@RequestBody Carrera carrera){
+        return carreraService.save(carrera);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Carrera> update(@RequestBody Carrera carrera, @PathVariable Long id){
+        return carreraService.update(carrera,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Carrera> delete(@PathVariable Long id){
+        return carreraService.delete(id);
+    }
+
+    @GetMapping("/t")
+    public Carrera t(){
+        return new Carrera();
+    }
+}
---
+++ src/main/java/com/universidadesapi/universidadesapi/service/CarreraService.java
@@ -0,0 +1,63 @@
+package com.universidadesapi.universidadesapi.service;
+
+import com.universidadesapi.universidadesapi.entity.Carrera;
+import com.universidadesapi.universidadesapi.repository.CarreraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarreraService {
+
+    @Autowired
+    CarreraRepository carreraRepository;
+
+
+    public List<Carrera> getAll(){
+        return carreraRepository.findAll();
+    }
+
+    public ResponseEntity<Carrera> getOne(Long id){
+        Optional<Carrera> find = carreraRepository.findById(id);
+
+        if(find.isEmpty())return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(find.get());
+    }
+
+
+    public ResponseEntity<Carrera> save(Carrera carrera){
+        if(carrera.getId() != null)return  ResponseEntity.badRequest().build();
+
+        Carrera result = carreraRepository.save(carrera);
+        return ResponseEntity.ok(result);
+    }
+
+    public ResponseEntity<Carrera> update(Carrera carrera, Long id){
+        if(carrera.getId() != null){
+            if(!carrera.getId().equals(id))return ResponseEntity.badRequest().build();
+        }
+
+        Optional<Carrera> find = carreraRepository.findById(id);
+        if(find.isEmpty())return ResponseEntity.notFound().build();
+
+        carrera.setUniversidad(find.get().getUniversidad());
+
+        Carrera result = carreraRepository.save(carrera);
+        return ResponseEntity.ok(result);
+    }
+
+
+
+    public ResponseEntity<Carrera> delete(Long id){
+        if(!carreraRepository.existsById(id))return ResponseEntity.notFound().build();
+
+        carreraRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
